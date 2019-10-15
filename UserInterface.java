55a2489069c2991d2e1a5b16c1aa7e38713dbfdf
Jack Bodine <jacktaylorbodine@gmail.com>
2019-10-15 11:51:10 -0700

    Initial sharing of project

---
+++ UserInterface.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class UserInterface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UserInterface
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void main()
+    {
+        
+    }
+}
