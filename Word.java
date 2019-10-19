f2a1b7ad4d9b02809c4e27f0a01ae40e9d99b0a8
jackbodine <jacktaylorbodine@gmail.com>
2019-10-18 17:48:23 -0700

    Added Classes

---
+++ Word.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Word here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Word
+{
+    // instance variables - replace the example below with your own
+    private String word;
+    private int score;
+
+    /**
+     * Constructor for objects of class Word
+     */
+    public Word()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
