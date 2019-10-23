b5453c2b07d93b53950066c08bbcb083331f395c
Jack <jacktaylorbodine@gmail.com>
2019-10-22 20:49:49 -0700

    fileReader

--- FileReader.java
+++ FileReader.java
@@ -15,48 +15,33 @@ public class FileReader
 {
     // instance variables - replace the example below with your own
 
+    private Scanner keyboard = new Scanner(System.in); 
+    protected static ArrayList<Word> words;
+    
     /**
      * Constructor for objects of class FileReader
      */
     public FileReader()
     {
-        // initialise instance variables
+       words = new ArrayList<>();
 
     }
 
     public static ArrayList<Word> train(String fileName, ArrayList<Word> words) throws FileNotFoundException{
+
         Scanner keyboard = new Scanner(System.in);  // Used to get keyboard input
         Scanner file = new Scanner(new File(fileName));
         while(file.hasNext()== true){
             String line = file.nextLine();
             String[] wordsInLine = line.split(" ");
-            for(int j = 1; j < (wordsInLine.length - 1); j++){
-
-                int character = wordsInLine[j].charAt(0);
-                if((65 <= character && character <= 90)||(97 <= character && character <= 122)){
-                    int location = -1;
-                    int score = Integer.parseInt(wordsInLine[0]);
-                    //Finds the location of word in words.
-                    location = Collections.binarySearch(words, new Word(wordsInLine[j]));
-                    if (location < 0){
-                        words.add(new Word(wordsInLine[j]));
-                        location = words.size() -1;
-                        //System.out.println("added a word");
-
-                        Collections.sort(words);
-                    } else {
-
-                    }
-
-                }
-            }
+            int score = Integer.parseInt(wordsInLine[0]);
+            words.add(new Word(wordsInLine[1], score));
+            
         }
         // For Testing Purposes.
         for(int i = 0; i < words.size(); i++){
-            System.out.println("Word: "+words.get(i).getName());
-            //System.out.println("TotleScore: "+words.get(i).getTotalScore());
-            System.out.println("AvgScore: "+words.get(i).getAvgScore());
-            //System.out.println("Occurences: "+words.get(i).getOccurences());
+            System.out.println("Word: "+words.get(i).getWord());
+            System.out.println("Score: "+words.get(i).getScore());
             System.out.println(" ");
 
         }
